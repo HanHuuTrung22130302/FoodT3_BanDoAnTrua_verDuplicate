9884d8a398c866bacd60875f69eea0d542684e17
Nguyen Anh Tuan <146004230+NguyenAnhTuan22130311@users.noreply.github.com>
2025-04-06 08:30:14 +0700

    Revert "Revert "feature/category-management""

--- src/main/java/hcmuaf/nlu/edu/vn/testproject/models/Category.java
+++ src/main/java/hcmuaf/nlu/edu/vn/testproject/models/Category.java
@@ -3,14 +3,16 @@ package hcmuaf.nlu.edu.vn.testproject.models;
 public class Category {
     private int categoryId;
     private String categoryName;
+    private String description;
 
     public Category() {
 
     }
 
-    public Category(int categoryId, String categoryName) {
+    public Category(int categoryId, String categoryName, String description) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+        this.description = description;
     }
 
     public int getCategoryId() {
@@ -27,5 +29,13 @@ public class Category {
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- src/main/java/hcmuaf/nlu/edu/vn/testproject/models/LogEntry.java
+++ src/main/java/hcmuaf/nlu/edu/vn/testproject/models/LogEntry.java
@@ -6,14 +6,16 @@ public class LogEntry {
     private Timestamp timestamp;
     private int accountId;
     private int roleId;
+    private String roleName;
     private String action;
     private String result;
     private String details;
 
-    public LogEntry(Timestamp timestamp, int accountId, int roleId, String action, String result, String details) {
+    public LogEntry(Timestamp timestamp, int accountId, int roleId, String roleName, String action, String result, String details) {
         this.timestamp = timestamp;
         this.accountId = accountId;
         this.roleId = roleId;
+        this.roleName = roleName;
         this.action = action;
         this.result = result;
         this.details = details;
@@ -26,6 +28,7 @@ public class LogEntry {
     public Timestamp getTimestamp() { return timestamp; }
     public int getAccountId() { return accountId; }
     public int getRoleId() { return roleId; }
+    public String getRoleName() { return roleName; }
     public String getAction() { return action; }
     public String getResult() { return result; }
     public String getDetails() { return details; }
