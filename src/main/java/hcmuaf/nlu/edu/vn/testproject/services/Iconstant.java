8772a4272fbbf73f32074c913c3ea901a891c4d9
Nguyen Anh Tuan <22130311@st.hcmuaf.edu.vn>
2025-05-25 18:54:41 +0700

    [#63]Fix: Loi 404 khi dang nhap voi google

--- src/main/java/hcmuaf/nlu/edu/vn/testproject/services/Iconstant.java
+++ src/main/java/hcmuaf/nlu/edu/vn/testproject/services/Iconstant.java
@@ -4,7 +4,7 @@ public class Iconstant {
     // Google
     public static final String GOOGLE_CLIENT_ID = "165264526065-7cooqsg6h3rda6ouocf2aq0fvqi5fcjh.apps.googleusercontent.com";
     public static final String GOOGLE_CLIENT_SECRET = "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";
-    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/testProject/loginGoogle";
+    public static final String GOOGLE_REDIRECT_URI = "http://foodt3.online/loginGoogle";
     public static final String GOOGLE_GRANT_TYPE = "authorization_code";
     public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
